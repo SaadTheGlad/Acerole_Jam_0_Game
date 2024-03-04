9fef0e9e1ba178a48b663141c51eec93e9a5ed97
SaadTheGlad <kakikebach1950@gmail.com>
2024-03-04 15:56:37 +0100

    Did some fancy coding

--- Scripts/XRayManager.cs
+++ Scripts/XRayManager.cs
@@ -8,10 +8,16 @@ public partial class XRayManager : Node
     [Export] public Node2D xRayArea;
     [Export] public Node2D skeleton;
     //Make an enum for all the stuff with children and use it in line 32 (if statement)
+    [Export] public BoneGroups boneGroups;
+    [Export] public Color tempHighlightColour;
+    [Export] public Color white;
 
     private Godot.Collections.Array<Sprite2D> arrayPublic = new Godot.Collections.Array<Sprite2D>();
     private Godot.Collections.Array<Sprite2D> array;
     private Godot.Collections.Array<Sprite2D> arrayFull;
+
+    bool hasScanned = false;
+    int currentIndex = -1000;
 
     public void StartScan()
     {
@@ -25,32 +31,80 @@ public partial class XRayManager : Node
 
     public void Scan()
     {
-        Node2D parent = (Node2D)skeleton.GetParent();
-        parent.Visible = true;
-        foreach(var node in skeleton.GetChildren())
+        if (true)
         {
-            if (node.Name == "Ribs")
+            if (currentIndex != -1000)
             {
-                foreach(var rib in node.GetChildren())
-                {
-                    Sprite2D ribSprite = (Sprite2D)rib;
-                    arrayPublic.Add(ribSprite);
-                }
-
+                arrayPublic[currentIndex].SelfModulate = white;
             }
+            
+            Node2D parent = (Node2D)skeleton.GetParent();
+            parent.Visible = true;
+            foreach (var node in skeleton.GetChildren())
+            {
+                if (true/*node.Name == boneGroups.ToString()*/)
+                {
+                    foreach (var bone in node.GetChildren())
+                    {
+                        if(bone is Sprite2D sprite)
+                        {
+                            sprite = (Sprite2D)bone;
+                            arrayPublic.Add(sprite);
+                            //GD.Print(sprite.Name);
+                        }
+                        else
+                        {
+                            GD.Print(bone.Name);
+                            foreach(var secondaryBone in bone.GetChildren())
+                            {
+                                if(secondaryBone is Sprite2D spriteSecondary)
+                                {
+                                    spriteSecondary = (Sprite2D)secondaryBone;
+                                    arrayPublic.Add(spriteSecondary);
+                                    //GD.Print(spriteSecondary.Name);
+                                }
+                            }
+                        }
+                    }
+
+                }
+                if(node is Sprite2D spriteHigher)
+                {
+                    arrayPublic.Add(spriteHigher);
+                    GD.Print(spriteHigher.Name);
+                }
+            }
+
+
+
+            array = arrayPublic.Duplicate();
+
+            arrayFull = array.Duplicate();
+            array.Shuffle();
+
+
+
+            int randomIndex = RandomSelection();
+            //while (arrayPublic[randomIndex].Name == "Sternum")
+            //{
+            //    randomIndex = RandomSelection();
+            //}
+            currentIndex = randomIndex;
+            Sprite2D current = arrayPublic[randomIndex];
+            current.SelfModulate = tempHighlightColour;
+
+
+            hasScanned = true;
         }
 
-        array = arrayPublic.Duplicate();
 
-        arrayFull = array.Duplicate();
-        array.Shuffle();
+        
 
-        RandomSelection();
     }
 
 
 
-    void RandomSelection()
+    int RandomSelection()
     {
         //Shuffle bag random selection
         if (array.Count == 0)
@@ -65,6 +119,24 @@ public partial class XRayManager : Node
 
 
         uint randomIndex = GD.Randi() % (uint)arrayPublic.Count;
-        GD.Print(arrayPublic[(int)randomIndex].Name);
+        return (int)randomIndex;
     }
+}
+
+public enum BoneGroups
+{ 
+    All,
+    Neck,
+    Skull,
+    MinuesOneBackbone,
+    Ribs,
+    Backbone,
+    Pelvis,
+    Left,
+    Right
+}
+
+public enum Avoid
+{
+    Sternum,
 }
